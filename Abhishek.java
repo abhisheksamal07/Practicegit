9f08fb8768bc1a59338f5071a61f1aec46810467
abhisheksamal07 <abhishek.samal10@gmail.com>
2022-09-19 22:03:06 +0530

    sum of two numbers

---
+++ Abhishek.java
@@ -0,0 +1,9 @@
+public class Abhishek {
+    public static void main(String[]args){
+        int a = 10, b = 20, sum;
+        sum = a+b;
+        System.out.println("the sum of two nubers "+ sum);
+        
+    }
+    
+}
